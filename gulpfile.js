662ba290ccd54e8728338cc55d3e399cbd87ad27
zmqcc <3052006651@qq.com>
2018-10-30 00:17:37 +0800

    首页导航基本完成

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('sass',function(){
+	return gulp.src("./sass/*.scss").pipe(sass()).pipe(rename({"suffix" : ".min"}))
+	//.pipe(cssnano())
+	.pipe(gulp.dest('dist/css'));
+})
+
+//压缩图片
+gulp.task('img',function(){
+	return gulp.src('./img/*').pipe(imagemin()).pipe(gulp.dest('dist/image'));
+})
+
+gulp.task('watch',function(){
+	gulp.watch('./sass/*.scss',['sass']);
+	gulp.watch('./img/*',['img']);
+})
